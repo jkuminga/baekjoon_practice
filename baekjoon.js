6d1cb0dacd3a7b1e43707c8cee0ab85aedeed989
jkuminga <dhj1318@gmail.com>
2025-10-05 14:19:56 +0900

    2720 세탁소 사장 동혁

--- baekjoon.js
+++ baekjoon.js
@@ -1,40 +1,49 @@
-// 진법 변환2
-// 과정
-// 1. n(10진수) 와 B(변환할 진법) 입력받음
-// 2. n보다 커지는 B의 a제곱에 대한 a를 구함
-// 3. a부터 시작해서 0 까지 B^a로 n을 나눔
+// 2720 세탁소  사장 동혁
+// 문제 : 케이스 수 (n) 만큼 반환해 줄 거스름돈을 입력 받아, 각 거스름돈을 쿼터, 다임, 니켈, 페니읙 개수를 반환
 
 const rl = require('readline').createInterface({
-    input :process.stdin, output: process.stdout
+    input : process.stdin, output : process.stdout
 });
 
-var digits = {}
-for(var i = 0 ; i <10; i++) digits[i.toString()] = i;
-for(var i = 0 ; i <26; i++) digits[(10+i).toString()] = String.fromCharCode(65+i);
+const qtr = 25, dime =10, nik = 5, pen = 1;
+var inputs =[];
 
+var totalResults = [];
 
-var n, b;
+rl.on('line', (line)=>{
+    inputs.push(parseInt(line))
+}).on('close', ()=>{
+    var num = inputs[0];
+    var changes = inputs.slice(1, num+1);
 
-rl.on('line',(line)=>{
-    [n,b] = line.split(' ').map(Number);
+    
 
-    rl.close();
-}).on('close',()=>{
-    var m = 0;
+    for(var i = 0; i<changes.length; i++){
+        var results = [0,0,0,0];
 
-    while(n > b**m){
-        // console.log(b**m);
-        m++;
+        while(changes[i] >= qtr){
+            changes[i] -= qtr;
+            results[0] += 1;
+            // console.log(`current change is ${changes[i]} and ${results[0]}`)
+        }
+
+        while(changes[i] >= dime){
+            changes[i] -= dime;
+            results[1] += 1;
+            // console.log(`current change is ${changes[i]} and ${results[1]}`)
+        }
+        while(changes[i] >= nik){
+            changes[i] -= nik;
+            results[2] += 1;
+            // console.log(`current change is ${changes[i]} and ${results[2]}`)
+        }
+        while(changes[i] >= pen){
+            changes[i] -= pen;
+            results[3] += 1;
+            // console.log(`current change is ${changes[i]} and ${results[3]}`)
+        }
+
+        console.log(...results)
     }
-    // console.log(`${n} is smaller than ${b**m}, and m is ${m}`)
 
-    var result = [];
-    for(var i = m-1; i >= 0; i--){
-        var mulB = Math.floor(n / b**i);
-        n = n - (mulB*b**i)
-        result.push(mulB)
-    }
-
-
-    console.log(result.map(e=>digits[e]).join(''));
 })
