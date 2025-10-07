3b90137ce805202800bf7355657b0ee51574309a
jkuminga <dhj1318@gmail.com>
2025-10-07 19:52:25 +0900

    2869 달팽이는 올라가고 싶다

--- baekjoon.js
+++ baekjoon.js
@@ -1,52 +1,38 @@
-// 1193 분수 찾기
-// 일단 현재 아이디어
-// 레이어 n 의 노드 개수 = (분자 + 분모) -1 개 = n 
-// 레이어 n의 진행방향 -> n이 짝수일 때 1/(n+1) 부터 (n+1) / 1 방향
-// 그래서 일단 타겟 넘버가 위치하는 레이어를 구한 후 그 레이어 위에서 배열을 구해서 리턴하는 방향으로 진행
-// ex) 13-> 5번 레이어 -> 11~15 까지 배열 나열 1/5, 2/4, 3/3, 4/2, 5/1
-
-
+// 2869 달팽이는 올라가고 싶다
+// 최초 제출 시에는 100 99 1000000000 입력에 대해서 시간 제한을 만족하지 못해서 실패
+// 해결방안 : 등차수열식 사용
+// n일동안 달팽이가 움직인 양 : An = (a-b)n
+// 이 때, n일의 아침에 a만큼 움직이게 되면, 실제로 -b는 진행되지 않는 이벤트가 된다.(정상 도착 시 하강 x)
+// 그러므로 an + b 가 v보다 커지는 가장 작은 n을 찾는 것이 이 문제의 해결 방법이 될 것이다.
+// 이 때 반복문은 최대한 지양해야 할 것이다.
 const rl = require('readline').createInterface({
-    input : process.stdin, output : process.stdout
+    input : process.stdin, output :process.stdout
 });
 
-function createFractionsList(layer){
-    var list = [];
-    for(var i = 0 ; i <layer; i ++){
-        list.push(`${i+1}/${layer - i}`)
-        // console.log(list)
-    }
-    if(layer% 2 == 1) list.reverse();
 
-    return list;
-}
+rl.on('line',(line)=>{
+    const [a, b, v] = line.split(' ').map(Number);
 
-rl.on('line', (line)=>{
-    var input = line.trim();
+    // var currentState = 0;
+    // var dayCount=0;
 
-    var currentLayer = 1;
-    var totalNodeCount = 1;
+    // while(true){
+    //     dayCount += 1;
+    //     // 1, a 만큼 올라가기
+    //     currentState += a;
+    //     // 2. a>=v면 break
+    //     if(currentState >= v) break;
+    //     // 3. b만큼 내려가기
+    //     currentState -= b
+    // };
+    // console.log(dayCount);
 
+    const n = Math.ceil((v - b) / (a-b));
+    // == An + b >= v
 
-    while(true){
-        if(input === '1') break;
-        currentLayer += 1;
-        totalNodeCount += currentLayer;
+    console.log(n);
 
-        // console.log(`현재 ${currentLayer}이고, 총 ${totalNodeCount}의 노드가 쌓여있습니다.`)
+    
 
-        if(totalNodeCount >= input) {
-            input = input - (totalNodeCount - currentLayer);
-            break;
-        }
-    }
+}) 
 
-    // console.log(`타겟 값 ${input}은 현재 레이어 ${currentLayer} 위에 위치하고 있습니다.`)
-
-    var currentLayerFractionsList = createFractionsList(currentLayer);
-
-    // console.log(currentLayerFractionsList);
-
-    // console.log(`${currentLayerFractionsList}의 ${input}값 찾기`)
-    console.log(currentLayerFractionsList[input-1]);
-})
