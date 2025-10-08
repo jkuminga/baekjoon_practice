04914c22f894f39a85cb7c3a88671a934fffd4cf
jkuminga <dhj1318@gmail.com>
2025-10-08 10:03:36 +0900

    2501 약수 구하기

--- baekjoon.js
+++ baekjoon.js
@@ -1,24 +1,21 @@
-/*
-<5086 배수와 약수 - 리펙토링>
-수정할 점
-- 요구사항에 따르면 0 0 을 입력받았을 때만 rl.close() 를 해주면 되는데, 
-  현재 상황에서는 3 3과 같은 상황에서는 종료되서는 안된다.
-
-*/  
+/* 
+<2501 약수 구하기>
+ */
 
 const rl = require('readline').createInterface({
-    input : process.stdin, output : process.stdout
+    input : process.stdin ,output : process.stdout
 });
 
+
 rl.on('line',(line)=>{
-    let [a, b] = line.split(' ').map(Number);
+    const [number, idx] = line.split(' ').map(Number);
 
-    // 변경사항 1: 입력이 0 0일 경우 종료
-    if(a === 0 && b === 0) rl.close();
+    var factors = []; 
 
+    for(var i = 1 ; i <=number; i++){
+        if(number % i === 0 ) factors.push(i);
+    }
 
-    // 변경사항 2 : 코드 단순화
-    if(b % a === 0) console.log('factor');
-    else if(a % b === 0) console.log('multiple');
-    else console.log('neither')
+    if(factors.length  < idx) console.log(0)
+    else console.log(factors[idx - 1]);
 })
