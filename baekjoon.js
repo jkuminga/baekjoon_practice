d24e7f45fe6545a5f8897ab0a5204654cdd2c51c
jkuminga <dhj1318@gmail.com>
2025-10-08 09:33:52 +0900

    5086 배수와 약수-리펙토링

--- baekjoon.js
+++ baekjoon.js
@@ -1,6 +1,10 @@
-// 5086 배수와 약수
-// 두 수 a b가 주어 질 때, 두 수 간의 약수/배수 관계를 찾는 문제
-// a가 b의 약수이면 factor, 배수이면 multiple, 어떠한 관계도 아니면 neither
+/*
+<5086 배수와 약수 - 리펙토링>
+수정할 점
+- 요구사항에 따르면 0 0 을 입력받았을 때만 rl.close() 를 해주면 되는데, 
+  현재 상황에서는 3 3과 같은 상황에서는 종료되서는 안된다.
+
+*/  
 
 const rl = require('readline').createInterface({
     input : process.stdin, output : process.stdout
@@ -9,13 +13,12 @@ const rl = require('readline').createInterface({
 rl.on('line',(line)=>{
     let [a, b] = line.split(' ').map(Number);
 
-    if(a > b){
-        if(a % b ==0) console.log('multiple');
-        else console.log('neither');
-    }else if(a < b){
-        if(b % a ==0) console.log('factor');
-        else console.log('neither');
-    }else{
-        rl.close();
-    }
+    // 변경사항 1: 입력이 0 0일 경우 종료
+    if(a === 0 && b === 0) rl.close();
+
+
+    // 변경사항 2 : 코드 단순화
+    if(b % a === 0) console.log('factor');
+    else if(a % b === 0) console.log('multiple');
+    else console.log('neither')
 })
