cf1cf9f57c8845bac69246142c219510d7563716
jkuminga <dhj1318@gmail.com>
2025-10-27 18:32:12 +0900

    3009 네번째 점

--- baekjoon.js
+++ baekjoon.js
@@ -1,32 +1,63 @@
 /* 
-<15894 - 수학은 체육과목입니다.>
+<3009 - 네번째 점>
 ▶︎개요
-- 한 변의 길이가 1인 정사각형을 피라미드 식으로 붙여 나갈 때, 가장 아랫부분의 정사각형의
-  개수가 n개가 됬을 때 피라미드 의 총 둘레의 길이를 구하기
+- 세 점이 주어졌을 때, 축에 평행한 직사각형을 만들기 위한 네번째 점의 좌표를 구하기
 
 ▶︎입력
-- 첫줄에 n 입력
+- 줄 마다 x,y 값을 입력
 
 ▶︎출력
-- 피라미드 전체의 둘레의 길이
+- 네 번째 점의 좌표를 출력
 
 ▶︎아이디어
-- an = n*4
+- 점들 중 각 축에 대해서 하나 있는 값이 네번째점의 좌표가 되야 함
 
 ▶︎개선방향
-- 영점과 좌표 사이 거리는 그냥 x, y로 두면 됨(x-0을 왜하노)
-- 변수 지정하지 말고 바로 Math.min() 안에 넣어서 콘솔에 출력
-- 입력 x,y 값이 w,h보다 작아서 abs 필요없음
 */
 
 const rl = require('readline').createInterface({
     input : process.stdin, output : process.stdout
 })
 
+let inputs = [];
+
 rl.on('line', (line)=>{
-    const n = Number(line);
+    inputs.push(line.split(' ').map(Number));
+}).on('close',()=>{
+    let result = [];
 
-    console.log(n*4);
+    if(inputs[2][0] === inputs[0][0]){
+        result.push(inputs[1][0])
+    }else if(inputs[2][0] === inputs[1][0]){
+        result.push(inputs[0][0])
+    }else{
+        result.push(inputs[2][0])
+    }
 
-    rl.close();
+    if(inputs[2][1] === inputs[0][1]){
+        result.push(inputs[1][1])
+    }else if(inputs[2][1] === inputs[1][1]){
+        result.push(inputs[0][1])
+    }else{
+        result.push(inputs[2][1])
+    }
+
+    console.log(result.join(' '));
 })
+
+
+// let xs={}, ys={};
+    
+//     for(var i = 0 ; i < inputs.length;i++){
+//         if(xs[inputs[i][0]] === undefined) {
+//             xs[inputs[i][0]] = 1;
+//         }else{
+//             xs[inputs[i][0]] = xs[inputs[i][0]] + 1;
+//         }
+
+//         if(xy[inputs[i][1]] === undefined) {
+//             xy[inputs[i][1]] = 1;
+//         }else{
+//             xy[inputs[i][1]] = xy[inputs[i][1]] + 1;
+//         }
+//     }
