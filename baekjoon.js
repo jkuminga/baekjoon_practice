a31a6e65571d6a75c6941e854ae72e2532c2427c
jkuminga <dhj1318@gmail.com>
2025-10-28 14:42:55 +0900

    50073 삼각형과 세 변

--- baekjoon.js
+++ baekjoon.js
@@ -1,18 +1,23 @@
 /* 
-<10101 - 삼각형 외우기>
+<50073 - 삼각형과 세 변>
 ▶︎개요
-- 세 각의 크기를 받아서 삼각형의 분류를 반환 
+- 세 변의 길이를 통해서 삼각형의 종류를 반환함
 
 ▶︎입력
-- 줄마다 한 각을 입력
+- 각 줄에 세 개의 변 개수를 모두 입력
+- 마지막 줄은 0 0 0, 이 줄은 계산하지 않음
 
 ▶︎출력
-- 세 각의 크기가 모두 60이면 Equilateral
-- 세 각의 합이 180이고, 두 각이 같은 경우에는 Isosceles
-- 세 각의 합이 180이고, 같은 각이 없는 경우에는 Scalene
-- 세 각의 합이 180이 아닌 경우에는 Error
+- 각 케이스에서 가장 긴 변의 길이보다 나머지 두변의 길이의 합이 길지 않으면 invalid
+- Equilateral :  세 변의 길이가 모두 같은 경우
+- Isosceles : 두 변의 길이만 같은 경우
+- Scalene : 세 변의 길이가 모두 다른 경우
  
 ▶︎아이디어
+- inputs[i]의 모든 변의 길이가 0이 아닌 동안 실행
+- 각 케이스에 대해 invalid 먼저 확인
+- filter 함수를 통해 길이가 다른 변만 result 리스트에 저장
+- result 의 길이를 통해 확인
 
 ▶︎개선방향
 */
@@ -22,25 +27,25 @@ const rl = require('readline').createInterface({
 })
 
 let inputs = [];
-rl.on('line',(line)=>{
-  inputs.push(parseInt(line));
-}).on('close',()=>{
-  // let sum = 0;
-  // inputs.forEach(v => sum = sum + v);
-  // const result = inputs.filter(v=>inputs.indexOf(v) === inputs.lastIndexOf(v));
 
-  let sum = 0 ; 
-  let result = [];
-  inputs.forEach((v)=>{
-    if(inputs.indexOf(v) === inputs.lastIndexOf(v)) result.push(v);
-    sum += v;
-  })
-  
-  if(sum == 180){
-    if(result.length === 0 ) console.log('Equilateral');
-    else if(result.length === 1) console.log('Isosceles');
-    else console.log('Scalene');
-  }else{
-    console.log('Error');
+rl.on('line', (line)=>{
+  inputs.push(line.split(' ').map(Number));
+}).on('close', ()=>{
+  for(let i of inputs){
+    if(i[0] === 0 && i[1] === 0 && i[2] === 0) {
+      return ;
+    }
+
+    if(2 * Math.max(...i) >= (i.reduce((acc, cur) => acc + cur, 0))){
+      console.log('Invalid');
+    }else{
+      if(i[0] === i[2] && i[1] === i[2]){
+        console.log('Equilateral');
+      }else if(i[0] === i[1] || i[0] === i[2] || i[2] === i[1] ){
+        console.log('Isosceles')
+      }else{
+        console.log('Scalene');
+      }
+    }
   }
 })
