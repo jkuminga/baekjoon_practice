7f5a2b36ef7eb5a624e414924425cef149f30ade
jkuminga <dhj1318@gmail.com>
2026-01-20 12:01:00 +0900

    2798 블랙잭 리팩토링

--- baekjoon.js
+++ baekjoon.js
@@ -14,7 +14,10 @@
 - 그냥 삼중반복문 쓰는거
 
 ▶︎개선방향
-
+- 하나의 fixed를 두고, 나머지 카드들 중 인덱스를 두개를 두고(left: 작은 쪽 부터/ right: 큰쪽부터)
+- 작은 값 + 큰값이 m보다 크면 right(큰 쪽)을 한 칸 뒤로(--)
+- 작은 값 + 큰 값이 m보다 작으면 best랑 비교 후 left를 한 칸 앞으로(++)
+- 이렇게 하면 기존의 O(n^3)에서 O(n^2)까지 줄일 수 있다.
 */
 
 const rl = require('readline').createInterface({
@@ -27,21 +30,26 @@ rl.on('line', (line)=>{
 }).on('close', ()=>{
   const [n,m] = inputs[0].split(' ').map(Number)
   var cards = inputs[1].split(' ').map(Number)
+  
   cards.sort((a,b)=>a-b)
 
   var best = 0;
 
-  cards.map((e, index)=>{
-    for(var f=index+1; f < cards.length ; f++){
-      for(var s = f+1; s< cards.length ; s++){
-        if(e + cards[f] + cards[s] <= m){
-          if(e + cards[f] + cards[s] > best){
-            best = e + cards[f] + cards[s];
-          }
-        }
+  for(var i = 0 ; i < n-2;i++){
+    let left = i+1;  // 가장 왼쪽(작은 쪽)
+    let right = n-1; // 가장 오른쪽(큰 쪽)
+
+    while (left < right){
+      const sum = cards[i] + cards[left] + cards[right]
+
+      if(sum > m){
+        right --
+      }else{
+        if(sum > best) best = sum
+        left ++
       }
     }
-  })
+  }
 
   console.log(best)
 })
