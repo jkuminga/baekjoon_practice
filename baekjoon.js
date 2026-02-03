b21c3723e44cf5a4259a5d7d67e9dc2503588e6d
jkuminga <dhj1318@gmail.com>
2026-02-03 16:25:24 +0900

    2839 설탕 배달 리펙토링(2중 루프->단일루프)

--- baekjoon.js
+++ baekjoon.js
@@ -14,6 +14,8 @@
 - 최대 5kg의 개수부터 시작해서 딱 맞아떨어지는 최소 봉지의 개수 찾기
 
 ▶︎개선방향
+- 단일 루프문으로 해결하자
+- 5kg 봉지 개수가 최대인 경우부터 반복문이 시작하기 때문에 N과 같아지는 상황에 break 해도 된다.
 
 */
 const rl = require('readline').createInterface({
@@ -24,18 +26,16 @@ const rl = require('readline').createInterface({
 rl.on('line', (line)=>{
   const N = Number(line.trim());
 
-  let answer = Infinity; 
+  let answer = -1; 
 
   let maxOfFives = Math.floor(N / 5);
-
-  for(let i = 0 ; i <= maxOfFives;i++){
-    for(let j = 0; 5*i + 3*j <= N; j ++){
-      if(5*i + 3*j === N){
-        if(i + j < answer) answer = i + j;
-      }
+  
+  for(var i = maxOfFives; i >=0; i--){
+    if((N - 5 * i) % 3 === 0){
+      answer = i + ((N - 5 * i ) / 3);
+      break;
     }
   }
 
-  (answer === Infinity) ? console.log(-1) : console.log(answer);
-
+  console.log(answer);
 })
