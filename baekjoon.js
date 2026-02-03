67b7b1b56d940ee5d23c8440c6310ddbeda1fba0
jkuminga <dhj1318@gmail.com>
2026-02-03 16:19:26 +0900

    2839 설탕 배달

--- baekjoon.js
+++ baekjoon.js
@@ -1,43 +1,41 @@
 /* 
-< 1018 - 체스판 다시 칠하기 > 
+< 2839 - 설탕배달 > 
 ▶︎개요
-- MN개의 단위 정사각형으로 나누어져 있는 MxN크기의 보드
-- 이 보드는 흰색/검은색이 랜덤하게 칠해져 있음
-- 8x8로 잘라서 체스판으로 만들고자 할 때, 다시 칠해야 하는 칸의 최소의 개수를 구하자
+- 5/3 kg의 설탕 봉지를 이용하여 최대한 적은 봉지를 들고 Nkg 설탕을 배달하고자 함
 
 ▶︎입력
-- 높이(N)와 너비(M) (M,N은 50 이하의 자연수)가 첫 줄에 주어짐
-- 두번째 줄부터 N개의 줄에는 보드의 각 행의 상태가 주어짐
+- Nkg
 
 ▶︎출력
-- 다시 칠해야 하는 칸의 최소 개수를 반환
+- 배달할 봉지의 최소 개수
+- 정확하게 Nkg을 맞출 수 없다면 -1
 
 ▶︎아이디어
-- 체스판을 칠하는 두가지 경우(왼쪽 최상단이 흰 or 흑)에 따라 최소 개수를 구함
-- 시작 칸의 색 = 인덱스 값의 합(i+j)이 짝수인 칸의 색
+- 최대 5kg의 개수부터 시작해서 딱 맞아떨어지는 최소 봉지의 개수 찾기
 
 ▶︎개선방향
+
 */
 const rl = require('readline').createInterface({
   input : process.stdin, output : process.stdout
 })
 
-let N;
 
 rl.on('line', (line)=>{
-  N = Number(line.trim());
-}).on('close',()=>{
-  let i = 0;
-  let answer;
+  const N = Number(line.trim());
 
-  while(N > 0){
-    if(i.toString().includes('666')){
-      answer = i;
-      N--;
-      // console.log(`current i is ${i} and current N is ${N} and current answer is ${answer}`)
+  let answer = Infinity; 
+
+  let maxOfFives = Math.floor(N / 5);
+
+  for(let i = 0 ; i <= maxOfFives;i++){
+    for(let j = 0; 5*i + 3*j <= N; j ++){
+      if(5*i + 3*j === N){
+        if(i + j < answer) answer = i + j;
+      }
     }
-    i++
   }
 
-  console.log(answer)
+  (answer === Infinity) ? console.log(-1) : console.log(answer);
+
 })
