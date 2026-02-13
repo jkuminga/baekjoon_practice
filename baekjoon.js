8b494ae00bc373d608062028e9424ec15336bfb5
jkuminga <dhj1318@gmail.com>
2026-02-13 15:52:46 +0900

    10814 나이순 정렬

--- baekjoon.js
+++ baekjoon.js
@@ -1,55 +1,53 @@
 /* ==============================================================================
-▶︎ 1181 - 단어 정렬
+▶︎ 10814 - 나이순 정렬
 
 ▶︎ 개요
-- 알파벳 소문자로 이루어진 N개의 단어가 들어오면 길이 순으로 정렬하고 길이가 같으면 사전으로 정렬하는 사전 프로그램 작성
-- 중복된 단어는 하나만 남기고 제거해야 함
+- 나이와 이름이 가입한 순서대로 주어질 때 나이는 증가하는 순으로, 나이가 같으면 가입한 사람이 먼저 오는 순서로 정렬
 
 ▶︎ 입력
-- 첫 줄에 단어의 개수 N (N <= 20,000)
-- 두 번째 줄부터 N개의 단어가 주어진다.(단어는 소문자이며 길이가 50을 넘지 않는다.)
+- 첫 줄에 회원 수 N이 주어진다.(N <= 500,000)
+- 두 번째 줄부터 N명의 회원의 나이와 이름이 공백으로 구분되어 주어진다.
+  (1<= 나이 <= 200, 이름은 알파벳 대소문자)
  
 ▶︎ 출력
-- 첫 줄부터 조건대로 나열된 단어들을 출력한다.
+- 첫 줄부터 n개의 줄에 걸쳐 온라인 저지 회원을 나이순, 나이가 같으면 가입한 순으로 한 줄에 한명씩 출력
 
 ▶︎ 아이디어
-- 단어의 길이가 최대 50이므로 동일한 길이의 단어가 들어갈 경우가 더 많을 것
-  -> 길이가 같은 단어 정렬부터 생각해보자.
+- 안정성이 보장되는 정렬(병합, 버블, 삽입) 사용하자.
 
 ▶︎ 개선방향
 ============================================================================== */
-// 1. 배열 전처리
+
 const fs = require('fs');
-const data = fs.readFileSync(0, 'utf8').trim().split(/\s+/);
-let n = Number(data[0]);
+const data = fs.readFileSync(0, 'utf8').split(/\s+/);
+
+const n = Number(data[0]);
 
 let arr = [];
-for(var i = 0 ; i < n ; i ++) arr.push(data[i + 1]);
+for(var i = 0; i < n; i++) arr.push([Number(data[2*i + 1]), data[2*i + 2]]);
 
-// 중복 없애기
-const arrToSet = new Set(arr);
-let uniqueArr = [...arrToSet];
-n = uniqueArr.length;
+let tmp = new Array(n);
 
-// 빈 배열 생성 - 각 요소에는 길이가 n인 단어들이 들어갈 것
-const tmp = Array.from({length: 50}, ()=>[]);
+for(let width = 1; width < n; width *=2){
+  for(let left = 0; left < n; left += width*2){
+    const mid = Math.min(left+width, n);
+    const right = Math.min(left+(2*width), n);
 
-for(var i = 0; i < n ; i ++){
-  tmp[uniqueArr[i].length - 1].push(uniqueArr[i]);
-}
+    let i = left, j = mid, k = left;
 
-for(var i = 0; i < 50 ; i++){
-  if(tmp[i].length !== null) tmp[i].sort();
-}
-
-
-let out = ''
-for(var i = 0 ; i < 50; i++){
-  if(tmp[i].length !== null){
-    for(var j = 0 ; j < tmp[i].length ; j++){
-      out += tmp[i][j] + '\n';
+    while(i < mid && j < right){
+      if(arr[i][0] <= arr[j][0]){
+        tmp[k++] = arr[i++];
+      }else{
+        tmp[k++] = arr[j++];
+      }
     }
+    while(i < mid) tmp[k++] = arr[i++];
+    while(j < right) tmp[k++] = arr[j++];
   }
+  const swap = arr; arr= tmp; tmp = swap;
 }
 
-process.stdout.write(out);
+let out = '';
+for(var i = 0 ; i < n - 1; i++) out += arr[i].join(' ') + '\n';
+console.log(out + arr[n-1].join(' '));
