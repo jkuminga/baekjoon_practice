5687814e4332831da043de677620163eb2aa2e79
jkuminga <dhj1318@gmail.com>
2026-02-24 15:09:05 +0900

    1269 대칭 차집합

--- baekjoon.js
+++ baekjoon.js
@@ -1,33 +1,40 @@
 /* ==============================================================================
-▶︎ 1764 - 듣보잡
+▶︎ 1269 - 대칭 차집합
 
 ▶︎ 개요
-듣도 못한 사람과 보도 못한 사람들의 명단을 받아서, 듣도 보도 못한 사람을 추출하자.
+자연수를 원소로 갖는 공집합이 아닌 집합 A,B가 주어질 때 두 집합의 대칭 차집합을 구하라
+(대칭 차집합 = (A-B) U (B-A))
 
 ▶︎ 입력
-- 첫째 줄에는 듣도 못한 사람의 수 N과 보도 못한 사람의 수 M이 공백으로 주어진다.[1, 500,000]
-- 둘째 줄부터 듣도 못한 사람 N명의 이름이 각 줄에 주어진다.
-- 그 다음 줄부터 보도 못한 사람 M명의 이름이 각 줄에 주어진다.
+- 첫째 줄에 집합 A의 원소 개수와 B의 원소 개수가 주어진다.[1, 200,000]
+- 둘째 줄에 A의 원소가 공백으로 구분되어 주어진다.(각 원소는 100,000,000 이하의 자연수)
+- 셋째 줄에 B의 원소가 공백으로 구분되어 주어진다.
 
 ▶︎ 출력
-- 첫째 줄부터 듣도 보도 못한 사람의 이름이 사전순으로 주어진다.
+- 대칭 차집합의 원소의 개수를 출력한다.
 
 ▶︎ 아이디어
 ============================================================================== */
 const fs = require('fs');
-const data = fs.readFileSync(0, 'utf8').trim().split(/\s+/)
+const data = fs.readFileSync(0, 'utf8').trim().split(/\s+/).map(Number);
 
-const N = Number(data[0]);
-const M = Number(data[1]);
-const unheard = new Set();
-for(var i = 0; i < N; i++) unheard.add(data[i+2]);
-const unseen = data.slice(N+2);
+const Asize = data[0];
+const Bsize = data[1];
+const setA = data.slice(2, Asize+2);
+const setB = data.slice(Asize+2);
+let AdiffB = new Set(data.slice(2, Asize+2));
+let BdiffA = new Set(data.slice(Asize+2));
 
-const ans = [];
-for(var u of unseen){
-  if(unheard.has(u)) ans.push(u);
+
+for(var i = 0 ; i < Bsize ;  i++){
+  if(AdiffB.has(setB[i])) AdiffB.delete(setB[i])
 }
 
-console.log(ans.length);
-console.log(ans.sort().join('\n'));
+for(var i = 0 ; i < Asize ; i++){
+  if(BdiffA.has(setA[i])) BdiffA.delete(setA[i]);
+}
+
+const union = new Set([...AdiffB, ...BdiffA]);
+console.log(union.size);
+
 
