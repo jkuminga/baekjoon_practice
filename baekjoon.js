8f0b45a5818faea3c97bffaacbe8be65ce33057a
jkuminga <dhj1318@gmail.com>
2026-02-24 15:23:11 +0900

    11478 서로 다른 부분 문자열이 개수

--- baekjoon.js
+++ baekjoon.js
@@ -1,40 +1,32 @@
 /* ==============================================================================
-▶︎ 1269 - 대칭 차집합
+▶︎ 11478 - 서로 다른 부분 문자열의 개수
 
 ▶︎ 개요
-자연수를 원소로 갖는 공집합이 아닌 집합 A,B가 주어질 때 두 집합의 대칭 차집합을 구하라
-(대칭 차집합 = (A-B) U (B-A))
+문자열 S에 대해서, S의 서로 다른 부분 문자열의 개수를 구하자
+(부분 문자열 : S에서 길이가 1보다 큰 연속된 일부분)
+(ex. ababc -> a,b,a,b,c,ab,ba,ab,bc,aba,bab,abc,abab,babc,ababc -> 중복 제외 12개)
 
 ▶︎ 입력
-- 첫째 줄에 집합 A의 원소 개수와 B의 원소 개수가 주어진다.[1, 200,000]
-- 둘째 줄에 A의 원소가 공백으로 구분되어 주어진다.(각 원소는 100,000,000 이하의 자연수)
-- 셋째 줄에 B의 원소가 공백으로 구분되어 주어진다.
+- 첫째 줄에 문자열 S가 주어짐(소문자, 길이는 1000 이하)
 
 ▶︎ 출력
-- 대칭 차집합의 원소의 개수를 출력한다.
+- S의 서로 다른 부분 문자열의 개수 출력.
 
 ▶︎ 아이디어
 ============================================================================== */
 const fs = require('fs');
-const data = fs.readFileSync(0, 'utf8').trim().split(/\s+/).map(Number);
+const s = fs.readFileSync(0, 'utf8').trim();
 
-const Asize = data[0];
-const Bsize = data[1];
-const setA = data.slice(2, Asize+2);
-const setB = data.slice(Asize+2);
-let AdiffB = new Set(data.slice(2, Asize+2));
-let BdiffA = new Set(data.slice(Asize+2));
+const ans = [];
 
-
-for(var i = 0 ; i < Bsize ;  i++){
-  if(AdiffB.has(setB[i])) AdiffB.delete(setB[i])
+for(let i = 1; i <= s.length; i++){
+  for(let j = 0; j < s.length - i + 1; j++){
+    ans.push(s.slice(j, j+i))
+  }
 }
 
-for(var i = 0 ; i < Asize ; i++){
-  if(BdiffA.has(setA[i])) BdiffA.delete(setA[i]);
-}
+console.log(new Set(ans).size);
 
-const union = new Set([...AdiffB, ...BdiffA]);
-console.log(union.size);
+
 
 
