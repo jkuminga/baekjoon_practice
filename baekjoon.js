08ce7bdc2a58240e390b4d3097b360a4af48442e
jkuminga <dhj1318@gmail.com>
2026-02-25 16:36:57 +0900

    1934 최소 공배수(똥냄새)

--- baekjoon.js
+++ baekjoon.js
@@ -1,32 +1,47 @@
 /* ==============================================================================
-▶︎ 11478 - 서로 다른 부분 문자열의 개수
+▶︎ 1934 - 최소공배수
 
 ▶︎ 개요
-문자열 S에 대해서, S의 서로 다른 부분 문자열의 개수를 구하자
-(부분 문자열 : S에서 길이가 1보다 큰 연속된 일부분)
-(ex. ababc -> a,b,a,b,c,ab,ba,ab,bc,aba,bab,abc,abab,babc,ababc -> 중복 제외 12개)
+두 자연수 A,B가 주어졌을 때, A와 B의 최소 공배수를 구하는 프로그램을 작성하시오.
 
 ▶︎ 입력
-- 첫째 줄에 문자열 S가 주어짐(소문자, 길이는 1000 이하)
+- 첫째 줄에 테스트 케이스의 개수 T[1, 1000]이 주어진다.
+- 두번째 줄 부터 T개 줄 만큼 A, B가 각 줄에 공백으로 구분되어 주어진다.[1, 45,000]
 
 ▶︎ 출력
-- S의 서로 다른 부분 문자열의 개수 출력.
+- 각 줄마다 A,B의 최소공배수를 한 줄에 하나씩 출력한다.
 
 ▶︎ 아이디어
+1) A,B 중 하나를 i배수로 늘리면서 다른 하나가 그 수와 같이 지는 경우를 찾는다.
+2) 최대 공약수를 구해서 A*B를 최대 공약수로 나눈다.
 ============================================================================== */
 const fs = require('fs');
-const s = fs.readFileSync(0, 'utf8').trim();
+const data = fs.readFileSync(0, 'utf8').trim().split(/\s+/).map(Number);
 
-const ans = [];
+const T = data[0];
 
-for(let i = 1; i <= s.length; i++){
-  for(let j = 0; j < s.length - i + 1; j++){
-    ans.push(s.slice(j, j+i))
+for(var i = 0 ; i < T; i++){
+  let [A,B] = [data[2*i + 1], data[2*i + 2]];
+
+  // 약수 먼저 구하기
+  let aFactors = [], bFactors = [];
+  for(var j = 0; j <= A; j++ ){
+    if(A % j === 0) aFactors.push(j);
   }
+  for(var j = 0 ; j <=B ; j++){
+    if(B % j === 0) bFactors.push(j);
+  }
+
+  aFactors.sort((a,b)=> (b-a));
+  bFactors = new Set(bFactors.sort((a,b)=> (b-a)));
+
+
+  let maxFactor;
+  // 최대 공약수 구하기
+  for(var j = aFactors.length -1; j >= 0; j--){
+    if(bFactors.has(aFactors[j])) maxFactor = aFactors[j];
+  }
+
+
+  console.log(A*B / maxFactor);
 }
-
-console.log(new Set(ans).size);
-
-
-
-
