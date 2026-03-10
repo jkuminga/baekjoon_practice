f312ac5e75e5dd4097e734158f8b900d4679af60
jkuminga <dhj1318@gmail.com>
2026-03-10 15:01:26 +0900

    4948 베르트랑 공준

--- baekjoon.js
+++ baekjoon.js
@@ -1,47 +1,52 @@
 /* ==============================================================================
-▶︎ 1929 - 소수 구하기
+▶︎ 4948 - 베르트랑 공준
 
 ▶︎ 개요
-M이상 N 이하의 소수를 모두 출력하는 프로그램 만들기
+베르트랑 공준이란 임의의 자연수 n에 대하여 n보다 크고 2n보다 작거나 같은 소수는 적어도 하나 존재한다는 내용
+자연수 n이 주어졌을 때 n보다 크고 2n보다 작거나 같은 소수의 개수를 구하자.
 
 ▶︎ 입력
-- 첫째 줄에 자연수 M, N이 주어진다.([1, 1,000,000], M이상 N이하의 소수가 하나 이상 존재하는 입력만 제공됨)
+- 입력은 여러 개의 테스트 케이스로 이루어져 있으며, 각 줄에 n이 주어진다.
+- 입력의 마지막에는 0이 주어진다.
 
 ▶︎ 출력
-- 한 줄에 하나씩, 증가하는 순서대로 소수를 출력
+- 각 케이스에 대해서 n보다 크고 2n보다 작거나 같은 소수의 개수를 출력한다.
 
 ▶︎ 아이디어
-1) 소수 판별 함수 생성
-2) 다음 소수를 찾는 함수를 생성 - 내부에서 ans에 push
-3) join('/n')으로 출력
 ============================================================================== */
-
 const fs = require('fs');
-const [M,N] = fs.readFileSync(0, 'utf8').trim().split(/\s+/).map(Number);
+const data = fs.readFileSync(0, 'utf8').trim().split(/\s+/).map(Number);
 
-const ans = []; // 답안 삽입 대열
+const ans = [];
 
-let currentNumber = M;
-if(currentNumber <= 2) ans.push(2);
-if(currentNumber % 2 === 0) currentNumber++;
+for(let num of data){
+  if(num === 0) break;
+  if(num === 1) ans.push(1);
+  else{
+    let count = 0;
+    const limit = 2*num;
+    num ++;
+    if(num % 2 === 0) num ++;
+
+    while(num <= limit){
+      if(isPrime(num)) count++
+      num += 2;
+    }
+
+    ans.push(count);
+  }
+}
 
 function isPrime(num){
   if(num < 2) return false;
   if(num === 2) return true;
   if(num % 2 === 0) return false;
-  
+
   const limit = Math.sqrt(num);
-  
-  for(let i = 3 ; i <= limit ; i++){
+  for(let i = 3; i <= limit ; i += 2){
     if(num % i === 0) return false;
   }
-
   return true;
-}
-
-while(currentNumber <= N){
-  if(isPrime(currentNumber)) ans.push(currentNumber);
-  currentNumber += 2;
 }
 
 console.log(ans.join('\n'));
