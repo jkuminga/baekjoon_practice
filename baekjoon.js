f3f4c0d252100afe33d6f7220a1cdf9593196d2d
jkuminga <dhj1318@gmail.com>
2026-03-16 17:12:11 +0900

    4949 균형잡힌 세상

--- baekjoon.js
+++ baekjoon.js
@@ -1,48 +1,61 @@
 /* ==============================================================================
-▶︎ 9012 - 괄호
+▶︎ 4949 - 균형 잡힌 세상 
+https://www.acmicpc.net/problem/4949
 
 ▶︎ 개요
-입력된 괄호 문자열이 올바른 괄호 문자열(VPS) 인지 아닌지를 판단하여 YES OR NO로 출력하자.
+문자열이 주어졌을 때, 괄호들의 균형이 잘 맞춰져 있는지 확인하는 프로그램을 작성하자.
+문자열에는 ()와 [] 두 종류의 괄호가 주어진다.
 
 ▶︎ 입력
-- 첫 번째 줄에 테스트 케이스의 개수 T가 주어진다.
-- 두 번째 줄부터 T개의 괄호 문자열이 각 줄에 주어진다.(문자열의 길이는 2 이상 50 이하)
+- 각 문자열은 영어 알파벳, 공백, 소괄호, 대괄호로 구성되며, 길이는 100글자보다 작거나 같으며, .으로 끝난다.
+- 입력의 종료 조건으로 . 하나가 들어온다.
 
 ▶︎ 출력
-- 각 문자열이 VPS이면 YES, 아니면 NO를 출력한다.
+- 각 줄마다 해당 문자열이 균형을 이루고 있으면 yes, 아니면 no를 출력한다.
 
 ▶︎ 아이디어
-1) (를 만나면 스택에 넣는다.
-2) )를 만나면 
-    - 스택이 비어 있으면 No 입력 후 다음 케이스 진행
-    - 스택이 비어있지 않으면 pop 후 다음 문자 테스트
-3) 각 케이스에 대해서
+- 모든 왼쪽 괄호를 스택에 넣는다.
+- 오른쪽 괄호를 만났을 때 스택을 pop 한 괄호의 타입이
+  1) 오른쪽 괄호의 타입과 같은 경우 : 계속 진행
+  2) 오른쪽 괄호의 타입과 다른 경우 : 불균형
+  3) 스택이 비어있는 경우 : 불균형
+- 또한 각 케이스 종료 후 스택이 비어 있는 경우에도 불균형
 ============================================================================== */
-
 const fs = require('fs');
-const inputs = fs.readFileSync(0, 'utf8').trim().split('\n');
+const input = fs.readFileSync(0, 'utf8').trim().split('\n');
 
-const k = Number(inputs[0]);
+const answers = [];
+const YES = 'yes';
+const NO = 'no';
 
+for(let str of input){
+  if(str === '.') break;
+  answers.push(vaildate(str));
+}
 
-const answer = [];
-
-for(var i = 1; i <= k ; i++){
+function vaildate(str){
   const stack = [];
-  const ps = inputs[i];
+  let isValid = true;
 
-  let notVPS = false;
-  for(const ch of ps){
-    if(ch === '('){
-      stack.push(ch);
-    }else if(ch === ')'){
-      if(stack.length === 0) {
-        notVPS = true;
-        break;
+  for(let ch of str){
+    if(ch === "(" || ch === "[") stack.push(ch);
+    else if(ch === ')') {
+      if(stack.length === 0) return NO;
+      else{
+        if(stack[stack.length - 1] === '(') stack.pop();
+        else return NO;
       }
-      stack.pop()
+    }
+    else if(ch === ']') {
+      if(stack.length === 0) return NO;
+      else{
+        if(stack[stack.length - 1] === '[') stack.pop();
+        else return NO;
+      }
     }
   }
-  answer.push((stack.length === 0 && !notVPS) ? 'YES' : 'NO');
+  if(stack.length === 0) return YES;
+  else return NO;
 }
-console.log(answer.join('\n'))
+
+console.log(answers.join('\n'));
