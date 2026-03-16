5b0df980bf8def3bdb2e17d2d523a578da3ef786
jkuminga <dhj1318@gmail.com>
2026-03-16 18:49:47 +0900

    12789 도키도키 간식드리미(디버깅)

--- baekjoon.js
+++ baekjoon.js
@@ -1,61 +1,81 @@
 /* ==============================================================================
-▶︎ 4949 - 균형 잡힌 세상 
-https://www.acmicpc.net/problem/4949
+▶︎ 12789 - 도키도키 간식드리미
+https://www.acmicpc.net/problem/12789
 
 ▶︎ 개요
-문자열이 주어졌을 때, 괄호들의 균형이 잘 맞춰져 있는지 확인하는 프로그램을 작성하자.
-문자열에는 ()와 [] 두 종류의 괄호가 주어진다.
+사람들이 번호표를 받고 한 줄로 서 있지만, 현재 순서는 섞여 있다.
+간식은 1번부터 차례대로 받아야 한다.
+
+직접 줄 안에서 순서를 바꿀 수는 없고,
+옆의 보조 대기줄(스택)에 사람을 잠시 이동시킬 수 있다.
+
+규칙
+1. 현재 줄의 맨 앞 사람은 바로 간식을 받거나,
+   보조 대기줄로 이동할 수 있다.
+2. 보조 대기줄에 들어간 사람은 다시 원래 줄로 돌아갈 수 없다.
+3. 보조 대기줄에서도 맨 앞(가장 최근에 들어간 사람)만 간식을 받을 수 있다.
+
+목표
+주어진 순서에서 모든 사람이 1번부터 N번까지
+순서대로 간식을 받을 수 있는지 판별한다.
 
 ▶︎ 입력
-- 각 문자열은 영어 알파벳, 공백, 소괄호, 대괄호로 구성되며, 길이는 100글자보다 작거나 같으며, .으로 끝난다.
-- 입력의 종료 조건으로 . 하나가 들어온다.
+- 첫번쨰 줄에는 내 앞에 있는 학생의 수 N이 주어진다(1과 1,000 사이의 자연수).
+- 다음 줄에는 내 앞에 서있는 학생들의 번호표 순서가 앞에서부터 뒤로 주어진다.(1,2,...,N)
 
 ▶︎ 출력
-- 각 줄마다 해당 문자열이 균형을 이루고 있으면 yes, 아니면 no를 출력한다.
+- 내가 간식을 무사히 받을 수 있으면 Nice, 아니면 Sad를 출력하자.
 
 ▶︎ 아이디어
-- 모든 왼쪽 괄호를 스택에 넣는다.
-- 오른쪽 괄호를 만났을 때 스택을 pop 한 괄호의 타입이
-  1) 오른쪽 괄호의 타입과 같은 경우 : 계속 진행
-  2) 오른쪽 괄호의 타입과 다른 경우 : 불균형
-  3) 스택이 비어있는 경우 : 불균형
-- 또한 각 케이스 종료 후 스택이 비어 있는 경우에도 불균형
+1) 현재 번호표(n)인 사람이 메인 대기열에 있다면, 그 사람을 찾을 때 까지 나머지 앞의 사람들을 스택에 푸쉬
+2) 다음 번호(n+1)인 사람이
+   A) 서브 대기열에 없다면  1)과 동일하게 진행
+   B) 서브 대기열(스택)에 있다면 pop 한 값이 n+1과 다르면 Sad
+3) 모두 진행 시 stack이 비어있다면 Nice
 ============================================================================== */
 const fs = require('fs');
-const input = fs.readFileSync(0, 'utf8').trim().split('\n');
+const input = fs.readFileSync(0, 'utf8').trim().split(/\s+/).map(Number);
 
-const answers = [];
-const YES = 'yes';
-const NO = 'no';
+const N = input[0];
+const line = input.slice(1).reverse();
+console.log(line);
 
-for(let str of input){
-  if(str === '.') break;
-  answers.push(vaildate(str));
-}
+const stack = [];
+let answer = 'Nice';
 
-function vaildate(str){
-  const stack = [];
-  let isValid = true;
-
-  for(let ch of str){
-    if(ch === "(" || ch === "[") stack.push(ch);
-    else if(ch === ')') {
-      if(stack.length === 0) return NO;
-      else{
-        if(stack[stack.length - 1] === '(') stack.pop();
-        else return NO;
-      }
-    }
-    else if(ch === ']') {
-      if(stack.length === 0) return NO;
-      else{
-        if(stack[stack.length - 1] === '[') stack.pop();
-        else return NO;
+for(let i = 1; i <= N; i++){
+  console.log(`=== 현재 번호 : ${i}번! ===`)
+  // 본 대기열에 i번 사람이 있다면, 
+  if(!stack.includes(i)){
+    console.log(`=== ${i}번은 메인 대기열에 있습니다. ===`)
+    for(let j = line.length - 1; j >= 0 ; j--){
+      if(line[j] === i) {
+        console.log(`   ${i} 번을 메인 대기열에서 찾았습니다!`)
+        line.pop();
+        console.log(`   현재 메인 대기열(역순) : ${line} / 서브 대기열 : ${stack}`);
+        break;
+      }else{
+        stack.push(line.pop());
+        console.log(`   ${i}번을 찾기 위해 ${line[j]} 를 임시 대기열로 보냅니다.`)
+        console.log(`   현재 메인 대기열(역순) : ${line} / 서브 대기열 : ${stack}`);
       }
     }
   }
-  if(stack.length === 0) return YES;
-  else return NO;
+  // 임시 대기열에 있다면
+  else{
+    console.log(`=== ${i}번은 서브 대기열에 있습니다. ===`)
+    if(stack[stack.length - 1] !== i){
+      console.log(`   ${i} 번은 임시 대기열에서 나올수 없습니다..`)
+      console.log(`   간식 받기에 실패했습니다...`);
+      answer = 'Sad';
+      break;
+    }else{
+      console.log(`   임시 대기열에서 ${i}번이 간식을 받습니다!`)
+      stack.pop();
+      console.log(`   현재 메인 대기열(역순) : ${line} / 서브 대기열 : ${stack}`);
+    }
+  }
 }
 
-console.log(answers.join('\n'));
+console.log('간식 받기에 성공했습니다!!');
+console.log(answer);
