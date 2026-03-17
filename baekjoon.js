a342ea1664d6f6f9b22e54d321afa7791e0f6182
jkuminga <dhj1318@gmail.com>
2026-03-17 17:34:34 +0900

    2164 카드2(변형)

--- baekjoon.js
+++ baekjoon.js
@@ -1,58 +1,56 @@
 /* ==============================================================================
-▶︎ 18258 - 큐2
-https://www.acmicpc.net/problem/18258
+▶︎ 2164 - 카드2
+https://www.acmicpc.net/problem/2164
 
 ▶︎ 개요
-정수를 조정하는 큐를 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하자.
-
-push X: 정수 X를 큐에 넣는 연산이다.
-pop: 큐에서 가장 앞에 있는 정수를 빼고, 그 수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-size: 큐에 들어있는 정수의 개수를 출력한다.
-empty: 큐가 비어있으면 1, 아니면 0을 출력한다.
-front: 큐의 가장 앞에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-back: 큐의 가장 뒤에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+1부터 N까지 번호가 붙은 N장의 카드가 1이 제일 위에 있고 순서대로 쌓여있다.
+제일 위에 있는 카드는 버리고, 그 뒤 제일 위에 있는 카드를 덱 제일 뒤로 옮긴다.
+카드가 한 장 남을 때 까지 동작을 반복한다. 제일 마지막에 남게 되는 카드를 구하시오.
 
 ▶︎ 입력
-- 첫번쨰 줄에는 명령의 수 N이 주어진다(1과 2,000,000 사이의 자연수)
-- 두번째 줄부터 명령이 하나씩 주어진다.(입력 정수는 1과 100,000 사이)
+- 첫째 줄에 정수 N(<=500,000)이 주어진다.
 
 ▶︎ 출력
-- 출력해야하는 명령이 주어질 때 마다 한 줄에 하나씩 출력한다.
+- 남게 되는 카드의 번호를 출력한다.
 
 ▶︎ 아이디어
 ============================================================================== */
 const fs = require('fs');
-const input = fs.readFileSync(0, 'utf8').trim().split('\n');
+const N = Number(fs.readFileSync(0, 'utf8').trim());
 
-const N = input[0];
+let answer;
 
-const queue = [];
-let front = 0; 
-let rear = 0;
 
-const answers = [];
-
-for(let i = 1; i <= N; i ++) {
-  let [command, value] = input[i].split(' ');
-  value = Number(value);
-  console.log(`=== 이번 명령은 ${command} 입니다 ===`);
-  if(command === 'push'){
-    queue.push(value);
+if(N === 1){
+  answer = 1;
+}else if(N === 2 || N === 3){
+  answer = 2;
+}else{
+  const queue = [];
+  let front = 0;
+  let rear = 0;
+  
+  for(let i = 1; i <= Math.floor(N / 2); i++){
+    queue.push(2*i);
     rear++;
-    console.log(`   push 후 스택 : ${queue}  |  front/rear : ${front}/${rear}`);
-  }else if(command === 'pop'){
-    answers.push((front === rear) ? -1 : queue[front++])
-  }else if(command === 'size'){
-    answers.push(rear - front);
-  }else if(command === 'empty'){
-    answers.push((front === rear) ? 1 : 0);
-  }else if(command === 'front'){
-    answers.push((front === rear) ? -1 : queue[front]);
-  }else if(command === 'back'){
-    answers.push((front === rear) ? -1 : queue[rear-1]);
   }
+  
+  if(N % 2 !== 0) {
+    queue[rear] = queue[front]
+    rear++; front++;
+  }
+
+  while(true){
+    front++;
+    if(rear - front === 1) break;
+
+    queue[rear] = queue[front]
+    rear++; front++;
+    // console.log(`queue : ${queue} front / rear :  ${front}/ ${rear}`);
+  }
+
+  answer = queue[rear-1]
 }
 
-
-console.log(answers.join('\n'));
+console.log(answer);
 
