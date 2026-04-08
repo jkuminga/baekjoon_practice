9ca8e434516161f8311d2575f740c21b6cd82aa6
jkuminga <dhj1318@gmail.com>
2026-04-08 16:10:04 +0900

    25501 재귀의 귀재

--- baekjoon.js
+++ baekjoon.js
@@ -1,25 +1,43 @@
 /* ==============================================================================
-▶︎ 27433 - 팩토리얼 2
-https://www.acmicpc.net/problem/27433
+▶︎ 25501 - 재귀의 귀재
+https://www.acmicpc.net/problem/25501
 
 ▶︎ 개요
-0보다 크거나 같은 정수 N 이 주어질 때 N!을 출력하는 프로그램 작성
+팰린드롬 : 앞에서 읽었을 때와 뒤에서 읽었을 때가 같은 문자열
+팰린드롬을 판별하는 재귀 함수를 이용하여 어떤 문자열이 팰린드롬인지의 여부와 재귀 함수 호출 횟수를 출력하자.
 
 ▶︎ 입력
-첫째 줄에 정수 N(0 ≤ N ≤ 20)이 주어진다.
+- 첫째 줄에 테스트케이스의 개수 T가 주어진다.(1 ~ 1,000)
+- 둘째 줄부터 T개의 줄에 알파벳 대문자로 구성된 문자열 S가 주어진다.(1 ~ 1 000)
 
 ▶︎ 출력
-첫째 줄에 N!을 출력한다.
+- 각 테스트케이스 마다 함수의 반환값과 recursion 함수의 호출 횟수를 각 줄에 출력
 
-▶︎ 아이디어
-재귀 함수 이용
+▶︎ 리팩토링 요소
+counter를 전역 변수로 사용하는 것이 부적절해 보임
+-> recursion의 파라미터로 제공하는 방식으로 변경
 ============================================================================== */
-const fs = require('fs')
-const N = Number(fs.readFileSync(0, 'utf8').trim());
+const fs = require('fs');
+const inputs = fs.readFileSync(0, 'utf8').trim().split('\n');
 
-function factorial(x){
-  if ( x <= 1 ) return 1;
-  return x * factorial(x - 1);
+const T = Number(inputs[0]);
+const answers = [];
+
+// 변경사항 : count를 매개변수로 받음
+function recursion(char, l, r, count){
+  if(l >= r) return [1, count];
+  if(char[l] != char[r]) return [0, count];
   
+  // recursion 재귀 시 마다 count 를 ++
+  return recursion(char, l+1, r-1, count + 1);
 }
-console.log(factorial(N));
+
+function isPalindrome(char){
+  return recursion(char, 0, char.length - 1, 1);
+}
+
+for(let i = 1; i <= T; i++ ){
+  const [result, count] = isPalindrome(inputs[i])
+  answers.push(`${result} ${count}`)
+}
+console.log(answers.join('\n'));
